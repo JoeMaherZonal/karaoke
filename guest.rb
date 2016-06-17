b331a768ee41519634c967f1d42430a623d33616
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-06-17 16:09:02 +0100

    remove guest by tel number

--- guest.rb
+++ guest.rb
@@ -13,4 +13,8 @@ class Guest
     @guests << new_guest
   end
 
+  def remove_guest_by_tel(tel_num)
+    @guests.each { |guest| @guests.delete(guest) if guest[:tel] == tel_num}
+  end
+
 end
--- specs/guest.rb
+++ specs/guest.rb
@@ -4,10 +4,18 @@ require_relative('../guest')
 class TestGuest < MiniTest::Test
   def setup()
     @guests = Guest.new()
+    @guests.add_guest("Rachel Barry", "123 Fake Street", "12345678910", "rachel_barry@gmail.com")
+    @guests.add_guest("Charlie Maher", "125 Cumberland Road", "07826173920", "charlie@cm.com")
   end
 
   def test_add_new_guest()
     @guests.add_guest("Joe Maher", "123 Fake Street", "07539271620", "joseph-maher@hotmail.co.uk")
+    assert_equal(3, @guests.guests.length )
+  end
+
+  def remove_guest_by_tel_num()
+    @guests.remove_guest_by_tel("12345678910")
     assert_equal(1, @guests.guests.length )
   end
+
 end
