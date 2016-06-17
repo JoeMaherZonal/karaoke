b0c78e64ec5bd1fb6783d860dfd365f8b3eee77e
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-06-17 16:03:10 +0100

    Add new guest

--- guest.rb
+++ guest.rb
@@ -1,2 +1,16 @@
 class Guest
+  attr_reader(:guests)
+  def initialize(guests = [])
+    @guests = guests
+  end
+
+  def add_guest(name, address, tel, email)
+    new_guest = {}
+    new_guest[:name] = name.to_s
+    new_guest[:address] = address.to_s
+    new_guest[:tel] = tel.to_s
+    new_guest[:email] = email.to_s
+    @guests << new_guest
+  end
+
 end
--- specs/guest.rb
+++ specs/guest.rb
@@ -5,4 +5,9 @@ class TestGuest < MiniTest::Test
   def setup()
     @guests = Guest.new()
   end
+
+  def test_add_new_guest()
+    @guests.add_guest("Joe Maher", "123 Fake Street", "07539271620", "joseph-maher@hotmail.co.uk")
+    assert_equal(1, @guests.guests.length )
+  end
 end
